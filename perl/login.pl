5ea9d7450049097f6042268b859acf06394b7a80
Marc Huffnagle <marc@element84.com>
2013-04-26 10:58:57 -0400

    Refactored the login.pl example

--- perl/login.pl
+++ perl/login.pl
@@ -1,77 +1,70 @@
 #!/usr/bin/env perl
 
-#
-# Note: This example uses the Net::Address::IP::Local, REST::Client, and
-# XML::Hash modules from CPAN.  You'll need to install those in order to run
-# the example code.
-#
-
 use strict;
 use warnings;
 
 use HTTP::Status qw(:constants);
 use Net::Address::IP::Local;
 use REST::Client;
-use XML::Hash;
+use XML::Simple;
 
-# The REST endpoint to create a token in the ECHO Testbed environment
-my $echo_rest_tokens_url = 'https://testbed.echo.nasa.gov/echo-rest/tokens';
+sub get_token {
+  my ($username, $password) = @_;
 
-# Create the REST client
-my $client = REST::Client->new();
+  # The REST endpoint to create a token in the ECHO Testbed environment
+  my $echo_rest_tokens_url = 'https://testbed.echo.nasa.gov/echo-rest/tokens';
 
-# We'll be using XML here.  We could also have used application/json
-my $request_headers = {
-  'Content-type' => 'application/xml',
-  'Accept' => 'application/xml'
-};
+  # Create the REST client
+  my $client = REST::Client->new();
 
-# Figure out our IP address, since that's required to get a token
-my $ip_address = Net::Address::IP::Local->public;
+  # We'll be using XML here.  We could also have used application/json
+  my $request_headers = {
+    'Content-type' => 'application/xml',
+    'Accept' => 'application/xml'
+  };
 
-# The token request being sent
-my $token_request_hash = {
-  'token' => {
-    'username' => { 'text' => 'guest' },
-    'password' => { 'text' => 'user@example.com' },
-    'client_id' => { 'text' => 'ETIM demo' },
-    'user_ip_address' => { 'text' => $ip_address }
-  }
-};
+  # Figure out our IP address, since that's required to get a token
+  my $ip_address = Net::Address::IP::Local->public;
 
-# For simplicity's sake, we'll use the XML::Hash module
-my $xml_converter = XML::Hash->new();
+  # The token request being sent
+  my $token_request_hash_ref = {
+    'username' => { 'content' => $username },
+    'password' => { 'content' => $password  },
+    'client_id' => { 'content' => 'ETIM demo' },
+    'user_ip_address' => { 'content' => $ip_address }
+  };
+  my $token_request_xml = XMLout($token_request_hash_ref, RootName => 'token');
 
-my $token_request_xml = $xml_converter->fromHashtoXMLString($token_request_hash);
+  # POST the request to the REST API
+  $client->POST($echo_rest_tokens_url, $token_request_xml, $request_headers);
 
-# POST the request to the REST API
-$client->POST($echo_rest_tokens_url, $token_request_xml, $request_headers);
+  my $token = undef;
 
-# Convert the XML response to something we can work with
-my $response_hash_ref = $xml_converter->fromXMLStringtoHash($client->responseContent());
-
-if ($client->responseCode() == HTTP_CREATED) {
-  # If we get a 201 status code back, the token was created
-  my $token = $response_hash_ref->{'token'}->{'id'}->{'text'};
-  print "Token: ${token}\n";
-}
-else {
-  # If we get anything other than a 201, something went wrong
-  print "Failed to create token\n";
-
-  # Extract and print out the errors.
-  my @errors = ();
-  if (ref($response_hash_ref->{'errors'}->{'error'}) eq 'HASH') {
-    push(@errors, $response_hash_ref->{'errors'}->{'error'}->{'text'});
+  if ($client->responseCode() == HTTP_CREATED) {
+    # If we get a 201 status code back, the token was created
+    my $response_hash_ref = XMLin($client->responseContent());
+    $token = $response_hash_ref->{'id'};
   }
   else {
-    foreach my $error (@{$response_hash_ref->{'errors'}->{'error'}}) {
-      push(@errors, $error->{'text'});
-    }
+    # If we get anything other than a 201, something went wrong
+    print "Failed to create token\n";
+    parse_and_display_errors($client->responseContent());
+    exit 1;
   }
 
-  foreach my $error (@errors) {
-    print "  - ${error}\n";
-  }
+  return $token;
 }
+
+# Pull out and display the errors
+sub parse_and_display_errors {
+  my $error_xml = shift;
+
+  my $errors_ref = XMLin($error_xml, ForceArray => qw(error));
+
+  foreach my $error (@{$errors_ref->{'error'}}) { print "  - ${error}\n"; }
+
+  return;
+}
+
+print 'Token: ' . get_token('guest', 'user@example.com') . "\n";
 
