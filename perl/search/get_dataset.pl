c781b85a960146d4734a1c84d98ec5946249141f
Marc Huffnagle <marc@element84.com>
2013-04-29 11:27:25 -0400

    Removed tokens from searches, and print out requests.

--- perl/search/get_dataset.pl
+++ perl/search/get_dataset.pl
@@ -8,63 +8,7 @@ use REST::Client;
 use URI::Encode qw(uri_encode);
 use XML::Simple;
 
-sub get_token {
-  my ($username, $password) = @_;
-
-  # The REST endpoint to create a token in the ECHO Testbed environment
-  my $echo_rest_tokens_url = 'https://testbed.echo.nasa.gov/echo-rest/tokens';
-
-  # Create the REST client
-  my $client = REST::Client->new();
-
-  # We'll be using XML here.  We could also have used application/json
-  my $request_headers = {
-    'Content-Type' => 'application/xml',
-    'Accept' => 'application/xml'
-  };
-
-  # The token request being sent
-  my $token_request_hash_ref = {
-    'username' => { 'content' => $username },
-    'password' => { 'content' => $password  },
-    'client_id' => { 'content' => 'ETIM demo' },
-    'user_ip_address' => { 'content' => '127.0.0.1' } 
-  };
-
-  my $token_request_xml = XMLout($token_request_hash_ref, RootName => 'token');
-
-  # POST the request to the REST API
-  $client->POST($echo_rest_tokens_url, $token_request_xml, $request_headers);
-
-  my $token = undef;
-
-  if ($client->responseCode() == HTTP_CREATED) {
-    # If we get a 201 status code back, the token was created
-    my $response_hash_ref = XMLin($client->responseContent());
-    $token = $response_hash_ref->{'id'};
-  }
-  else {
-    # If we get anything other than a 201, something went wrong
-    print "Failed to create token\n";
-    parse_and_display_errors($client->responseContent());
-    exit 1;
-  }
-
-  return $token;
-}
-
-sub parse_and_display_errors {
-  my $error_xml = shift;
-
-  my $errors_ref = XMLin($error_xml, ForceArray => qw(error));
-
-  foreach my $error (@{$errors_ref->{'error'}}) { print "  - ${error}\n"; }
-
-  return;
-}
-
 sub get_dataset_id {
-  my $token = shift;
   my $datasets_url = 'https://testbed.echo.nasa.gov/catalog-rest/echo_catalog/datasets';
 
   # Build up the list of search parameters that we're using
@@ -83,17 +27,16 @@ sub get_dataset_id {
   # We'll be using XML here.  We could also have used application/json
   my $request_headers = {
     'Accept' => 'application/xml',
-    'Echo-Token'=> $token
   };
 
   # Issue the GET request
+  print "> GET ${request_url}\n";
   $client->GET($request_url, $request_headers);
 
   my $response_hash_ref = XMLin($client->responseContent());
   return $response_hash_ref->{'reference'}->{'id'};
 }
 
-my $token = get_token('guest', 'woot');
-my $dataset_id = get_dataset_id($token);
+my $dataset_id = get_dataset_id();
 print "${dataset_id}\n";
 
